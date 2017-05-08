0aed866c40067773c32cc9d46edd39bf552a5728
Aditya Nanjangud <aditya.ngud@gmail.com>
2017-05-07 19:15:34 -0500

    Ensemble Classifier with Basic Features

---
+++ features.py
@@ -0,0 +1,79 @@
+from sklearn.base import BaseEstimator, TransformerMixin
+import numpy as np
+
+class Sex(BaseEstimator, TransformerMixin):
+    def get_feature_names(self):
+        return np.array(['sex'])
+
+    def fit(self, documents, y=None):
+        return self
+
+    def transform(self, documents):
+        sex_value = lambda l1: 1 if l1 == "male" else 2
+        sex = [sex_value(d) for d in documents]
+        return np.array([sex]).T
+
+class Embarked(BaseEstimator, TransformerMixin):
+    def get_feature_names(self):
+        return np.array(['embarked'])
+
+    def fit(self, documents, y=None):
+        return self
+
+    def transform(self, documents):
+        embarked = []
+        for d in documents:
+            if d == "C":
+                value = 1
+            elif d == "S":
+                value = 2
+            else:
+                value = 3
+            embarked.append(value)
+
+        return np.array([embarked]).T
+
+class Pclass(BaseEstimator, TransformerMixin):
+    def get_feature_names(self):
+        return np.array(['pclass'])
+
+    def fit(self, documents, y=None):
+        return self
+
+    def transform(self, documents):
+        pclass = [d for d in documents]
+        return np.array([pclass]).T
+
+class SibSp(BaseEstimator, TransformerMixin):
+    def get_feature_names(self):
+        return np.array(['sibsp'])
+
+    def fit(self, documents, y=None):
+        return self
+
+    def transform(self, documents):
+        sibsp = [d for d in documents]
+        return np.array([sibsp]).T
+
+class Parch(BaseEstimator, TransformerMixin):
+    def get_feature_names(self):
+        return np.array(['parch'])
+
+    def fit(self, documents, y=None):
+        return self
+
+    def transform(self, documents):
+        parch = [d for d in documents]
+        return np.array([parch]).T
+
+class Fare(BaseEstimator, TransformerMixin):
+    def get_feature_names(self):
+        return np.array(['fare'])
+
+    def fit(self, documents, y=None):
+        return self
+
+    def transform(self, documents):
+        fare = [d for d in documents]
+        return np.array([fare]).T
+
---
+++ titanic.py
@@ -0,0 +1,109 @@
+import pandas as pd
+import logging
+import time
+import numpy as np
+from features import Sex, Embarked, Parch, SibSp, Pclass
+from sklearn import svm
+from sklearn.ensemble import RandomForestClassifier, VotingClassifier
+from sklearn.linear_model import LogisticRegression
+from sklearn.model_selection import GridSearchCV
+
+logging.basicConfig(format='%(asctime)s %(levelname)s: %(message)s', level=logging.INFO)
+start_time = time.time()
+logging.info(" ----------- Start  ------------")
+
+train_data = pd.read_csv('train.csv')
+test_data = pd.read_csv('test.csv')
+test_y = pd.read_csv('gender_submission.csv')
+test_y = np.array(test_y.Survived)
+
+train_pclass = np.array(train_data.Pclass)
+train_sex = np.array(train_data.Sex)
+train_sibsp = np.array(train_data.SibSp)
+train_parch = np.array(train_data.Parch)
+#train_ticket = np.array(train_data.Ticket)
+train_fare = np.array(train_data.Fare)
+train_embarked = np.array(train_data.Embarked)
+
+
+test_pclass = np.array(test_data.Pclass)
+test_sex = np.array(test_data.Sex)
+test_sibsp = np.array(test_data.SibSp)
+test_parch = np.array(test_data.Parch)
+#train_ticket = np.array(train_data.Ticket)
+test_fare = np.array(test_data.Fare)
+test_embarked = np.array(test_data.Embarked)
+
+'''
+train_cabin = list(train_data.Cabin)
+for i in xrange(train_cabin):
+    if pd.isnull(train_cabin[i]):
+        train_cabin[i] = ""
+train_cabin = np.array(train_cabin)
+'''
+
+train_y = np.array(train_data.Survived)
+
+training_feature_vectors = []
+testing_feature_vectors = []
+
+sex = Sex()
+train_x = sex.fit_transform(train_sex)
+training_feature_vectors.append(train_x)
+test_x = sex.fit_transform(test_sex)
+testing_feature_vectors.append(test_x)
+
+sibsp = SibSp()
+train_x = sibsp.fit_transform(train_sibsp)
+training_feature_vectors.append(train_x)
+test_x = sibsp.fit_transform(test_sibsp)
+testing_feature_vectors.append(test_x)
+
+parch = Parch()
+train_x = parch.fit_transform(train_parch)
+training_feature_vectors.append(train_x)
+test_x = parch.fit_transform(test_parch)
+testing_feature_vectors.append(test_x)
+
+
+embarked = Embarked()
+train_x = embarked.fit_transform(train_embarked)
+training_feature_vectors.append(train_x)
+test_x = embarked.fit_transform(test_embarked)
+testing_feature_vectors.append(test_x)
+
+
+pclass = Pclass()
+train_x = pclass.fit_transform(train_pclass)
+training_feature_vectors.append(train_x)
+test_x = pclass.fit_transform(test_pclass)
+testing_feature_vectors.append(test_x)
+
+
+final_training_feature_vectors = np.hstack(training_feature_vectors)
+final_testing_feature_vectors = np.hstack(testing_feature_vectors)
+
+svm = svm.SVC(C=0.3,kernel='linear', probability=True)
+rfc = RandomForestClassifier(random_state=1, n_estimators=100)
+lr = LogisticRegression(random_state=1)
+eclf = VotingClassifier(estimators=[
+    ('svm', svm), ('lr', lr), ('rfc', rfc)
+], voting='soft', weights=[0.3, 0.3, 0.4])
+
+pg = {'svm__C': [0.01, 0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9, 1, 1.1, 1.2], 'lr__C': [1.0, 100.0],\
+      'rfc__n_estimators': [20, 100]}
+
+grid = GridSearchCV(estimator=eclf, param_grid=pg, cv=5, n_jobs=4, verbose=5)
+grid.fit(final_training_feature_vectors, train_y)
+print grid.best_params_
+print grid.best_score_
+print "grid score: ", grid.score(final_testing_feature_vectors, test_y)
+
+#eclf.fit(final_training_feature_vectors, train_y)
+#print eclf.score(final_testing_feature_vectors, test_y)
+
+logging.info(" ----------- End  ------------")
+total_time = time.time() - start_time
+m, s = divmod(total_time, 60)
+h, m = divmod(m, 60)
+print "Program run time: %d:%02d:%02d" % (h, m, s)
